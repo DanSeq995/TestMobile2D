33bc9a59e092bd1a6000293bdef2a2d083f289b7
DanSuperSoup <danieleromano1995@gmail.com>
2023-06-17 20:49:47 +0200

    Update spawntime

--- Assets/Scripts/SpawnCoordinator.cs
+++ Assets/Scripts/SpawnCoordinator.cs
@@ -1,29 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnCoordinator : MonoBehaviour
 {
+    public double spawnTime = 0;
+    public float spawnSpeed = 2.5f;
     // Start is called before the first frame update
-
-    void spawnRoutine() {
-        GameObject[] spawnPoints = new GameObject[] {};
-       
-        var spawnMax = 2;
-        for(var spawnCount = 0; spawnCount < spawnMax; spawnCount++) {
-            var spawnIndex = Random.Range(0, transform.childCount);
-            var spawnPoint = transform.GetChild(spawnIndex);
-            spawnPoint.GetComponent<SpawnPointController>().activateSpawn();
-        }
-    }
     void Start()
     {
-        InvokeRepeating("spawnRoutine", 0f, 3f);
+        InvokeRepeating("difficultyChange", 0f, 10f);
+        spawnRoutine();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        spawnTime += Time.deltaTime;
+    }
+
+    void difficultyChange(){
+        if(spawnSpeed >= 1.2){
+            spawnSpeed -= 0.2f;
+            print("SpawnSpeed: " + spawnSpeed);
+            }
+    }
+    void spawnRoutine()
+    {
+        GameObject[] spawnPoints = new GameObject[] { };
+
+        var spawnMax = 2;
+        for (var spawnCount = 0; spawnCount < spawnMax; spawnCount++)
+        {
+            var spawnIndex = UnityEngine.Random.Range(0, transform.childCount);
+            var spawnPoint = transform.GetChild(spawnIndex);
+            spawnPoint.GetComponent<SpawnPointController>().activateSpawn();
+        }
+        Invoke("spawnRoutine", spawnSpeed);
     }
 }
